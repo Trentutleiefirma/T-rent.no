63811c2d9707c68440477603f39f51d9daa38b9a
Trentutleiefirma <t.rent.utleiefirma@gmail.com>
2026-08-22 09:24:01 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,265 +1,393 @@
 <?php
 
-// Enqueue parent and child styles
-function construction_equipments_child_enqueue_styles() {
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 
-    // 1. Load parent theme CSS
-    wp_enqueue_style(
-        'land-excavator-style', // Parent handle
-        get_template_directory_uri() . '/style.css',
-        array(),
-        LAND_EXCAVATOR_VERSION
-    );
-
-    // 2. Load child theme CSS (depends on parent)
-    wp_enqueue_style(
-        'land-excavator-child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('land-excavator-style'),
-        wp_get_theme()->get('Version')
-    );
-}
-add_action( 'wp_enqueue_scripts', 'construction_equipments_child_enqueue_styles' );
-
-// Enqueue child theme JS in addition to parent JS
-function construction_equipments_child_scripts() {
-
-    // Load child custom JS
-    wp_enqueue_script(
-        'land-excavator-child-js',
-        get_stylesheet_directory_uri() . '/revolution/assets/js/custom.js', // <-- your file in child theme
-        array('jquery'), // load after jQuery
-        wp_get_theme()->get('Version'),
-        true
-    );
-}
-add_action( 'wp_enqueue_scripts', 'construction_equipments_child_scripts' );
-
-// Theme setup
-if (!function_exists('construction_equipments_setup')) :
-    function construction_equipments_setup() {
-        add_theme_support('automatic-feed-links');
-        add_theme_support('title-tag');
-        add_theme_support('custom-header');
-        add_theme_support('responsive-embeds');
-        add_theme_support('post-thumbnails');
-        add_theme_support('align-wide');
-        add_editor_style(array('assets/css/editor-style.css'));
-        add_theme_support('custom-background', apply_filters('construction_equipments_custom_background_args', array(
-            'default-color' => 'ffffff',
-            'default-image' => '',
-        )));
-
-        if ( ! defined( 'LAND_EXCAVATOR_IMPORT_URL' ) ) {
-            define( 'LAND_EXCAVATOR_IMPORT_URL', esc_url( admin_url( 'themes.php?page=constructionequipments-demoimport' ) ) );
-        }
-        if ( ! defined( 'LAND_EXCAVATOR_WELCOME_MESSAGE' ) ) {
-            define( 'LAND_EXCAVATOR_WELCOME_MESSAGE', __( 'Welcome to Construction Equipments', 'construction-equipments' ) );
-        }
-
-    }
-endif;
-add_action('after_setup_theme', 'construction_equipments_setup');
-
-// Set content width
-function construction_equipments_content_width() {
-    $GLOBALS['content_width'] = apply_filters('construction_equipments_content_width', 1170);
-}
-add_action('after_setup_theme', 'construction_equipments_content_width', 0);
-
-// Register widget areas
-function construction_equipments_widgets_init() {
-    register_sidebar(array(
-        'name' => __('Sidebar Widget Area', 'construction-equipments'),
-        'id' => 'land-excavator-sidebar-primary',
-        'description' => __('The Primary Widget Area', 'construction-equipments'),
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget' => '</aside>',
-        'before_title' => '<h4 class="widget-title">',
-        'after_title' => '</h4><div class="title"><span class="shap"></span></div>',
-    ));
-    register_sidebar(array(
-        'name' => __('Footer Widget Area', 'construction-equipments'),
-        'id' => 'land-excavator-footer-widget-area',
-        'description' => __('The Footer Widget Area', 'construction-equipments'),
-        'before_widget' => '<div class="footer-widget col-lg-3 col-sm-6 wow fadeIn" data-wow-delay="0.2s"><aside id="%1$s" class="widget %2$s">',
-        'after_widget' => '</aside></div>',
-        'before_title' => '<h5 class="widget-title w-title">',
-        'after_title' => '</h5><span class="shap"></span>',
-    ));
-}
-add_action('widgets_init', 'construction_equipments_widgets_init');
-
-// Remove customizer settings
-function construction_equipments_remove_custom($wp_customize) {
-
-    $wp_customize->remove_setting('land_excavator_primary_color');
-    $wp_customize->remove_control('land_excavator_primary_color');
-
-    $wp_customize->remove_setting('land_excavator_header_info_email');
-    $wp_customize->remove_control('land_excavator_header_info_email');
-
-    $wp_customize->remove_setting('land_excavator_header_mail_icon');
-    $wp_customize->remove_control('land_excavator_header_mail_icon');
-    
-    $wp_customize->remove_section( 'land_excavator_projetcs_sec' );
-
-}
-add_action('customize_register', 'construction_equipments_remove_custom', 1000);
-
-function construction_equipments_child_customize_register( $wp_customize ) {
-
-    $wp_customize->add_setting( 'construction_equipments_primary_color',
-        array(
-        'default'           => '#EDB509',
-        'capability'        => 'edit_theme_options',
-        'sanitize_callback' => 'sanitize_hex_color',
-        )
-    );
-    $wp_customize->add_control( 
-        new WP_Customize_Color_Control( 
-        $wp_customize, 
-        'construction_equipments_primary_color',
-        array(
-            'label'      => esc_html__( 'Primary Color', 'construction-equipments' ),
-            'section'    => 'land_excavator_global_color_section',
-            'settings'   => 'construction_equipments_primary_color',
-        ) ) 
-    );
-
-    /*
-    * Customizer feature bike section
-    */
-    /*Project Options*/
-    $wp_customize->add_section('construction_equipments_products_section', array(
-        'priority'       => 5,
-        'capability'     => 'edit_theme_options',
-        'theme_supports' => '',
-        'title'          => __('Our Product Options', 'construction-equipments'),
-        'panel'       => 'land_excavator_panel',
-    ));
-
-    /*Project Enable Option*/
-    $wp_customize->add_setting(
-        'construction_equipments_enable_product_section',
-        array(
-            'capability'        => 'edit_theme_options',
-            'transport'         => 'refresh',
-            'default'           => 1,
-            'sanitize_callback' => 'land_excavator_sanitize_checkbox',
-        )
-    );
-
-    $wp_customize->add_control(
-        'construction_equipments_enable_product_section',
-        array(
-            'label'       => __('Enable Product Category Section', 'construction-equipments'),
-            'description' => __('Checked to show the product section', 'construction-equipments'),
-            'section'     => 'construction_equipments_products_section',
-            'type'        => 'checkbox',
-        )
-    );
-
-    /*Product Section Heading*/
-    $wp_customize->add_setting(
-        'construction_equipments_projetcs_main_text',
-        array(
-            'capability'        => 'edit_theme_options',
-            'transport'         => 'refresh',
-            'default'           => '',
-            'sanitize_callback' => 'sanitize_text_field',
-        )
-    );
-    $wp_customize->add_control(
-        'construction_equipments_projetcs_main_text',
-        array(
-            'label'       => __('Edit Section Heading', 'construction-equipments'),
-            'description' => __('Edit product section heading', 'construction-equipments'),
-            'section'     => 'construction_equipments_products_section',
-            'type'        => 'text',
-        )
-    );
-
-    /*Product Section Heading*/
-    $wp_customize->add_setting(
-        'construction_equipments_projetcs_main_heading',
-        array(
-            'capability'        => 'edit_theme_options',
-            'transport'         => 'refresh',
-            'default'           => '',
-            'sanitize_callback' => 'sanitize_text_field',
-        )
-    );
-    $wp_customize->add_control(
-        'construction_equipments_projetcs_main_heading',
-        array(
-            'label'       => __('Edit Section Heading', 'construction-equipments'),
-            'description' => __('Edit product section heading', 'construction-equipments'),
-            'section'     => 'construction_equipments_products_section',
-            'type'        => 'text',
-        )
-    );
-
-    $args = array(
-       'type'      => 'product',
-        'taxonomy' => 'product_cat'
-    );
-    $categories = get_categories($args);
-        $cat_posts = array();
-            $i = 0;
-            $cat_posts[]='Select';
-        foreach($categories as $category){
-            if($i==0){
-            $default = $category->slug;
-            $i++;
-        }
-        $cat_posts[$category->slug] = $category->name;
+/**
+ * is_rental_product
+ *
+ * @param mixed $product_id
+ *
+ * @return boolean
+ */
+function is_rental_product($product_id)
+{
+    if (is_shop()) {
+        return false;
     }
 
-    $wp_customize->add_setting('construction_equipments_product_category',array(
-        'sanitize_callback' => 'land_excavator_sanitize_choices',
-    ));
-    $wp_customize->add_control('construction_equipments_product_category',array(
-        'type'    => 'select',
-        'choices' => $cat_posts,
-        'label' => __('Select Product Category','construction-equipments'),
-        'section' => 'construction_equipments_products_section',
-    ));
+    $is_product = wc_get_product($product_id);
+    $product_type = $is_product ? $is_product->get_type() : '';
 
-    $wp_customize->add_setting( 
-		'land_excavator_product_settings_upgraded_features',
-		array(
-			'sanitize_callback' => 'sanitize_text_field'
-		)
-	);
-
-	$wp_customize->add_control(
-		'land_excavator_product_settings_upgraded_features', 
-		array(
-			'type'=> 'hidden',
-			'description' => "
-				<div class='customizer-upgraded-features'>
-					<span class='pro-head'>Want more product options?</span><br/>
-					<span class='pro-text'>						
-						<a target='_blank' href='". esc_url(LAND_EXCAVATOR_BUY_NOW) ." '>Check Land Excavator PRO version.</a>
-					</div>
-				</div>
-			",
-			'section' => 'construction_equipments_products_section'
-		)
-	);
-
+    return $product_type && $product_type === 'redq_rental' ? true : false;
 }
-add_action( 'customize_register', 'construction_equipments_child_customize_register', 20 );
 
-// Global colors
-function construction_equipments_dynamic_colors() {
-    $construction_equipments_primary_color = get_theme_mod('construction_equipments_primary_color', '#EDB509');
-
-    $construction_equipments_custom_css = ':root {';
-    $construction_equipments_custom_css .= '--primary-color: ' . esc_attr( $construction_equipments_primary_color ) . ';';
-    $construction_equipments_custom_css .= '}';
-
-    echo '<style type="text/css">' . $construction_equipments_custom_css . '</style>';
+/**
+ * Convert Euro To Standard
+ *
+ * @param $date
+ * @param $euro_format
+ * @return false|string
+ * @since 1.0.0
+ */
+function rnb_generalized_date_format($date, $euro_format)
+{
+    $formatted_date = $euro_format === 'no' ? $date : strtotime(str_replace('/', '.', $date));
+    return (new Carbon($formatted_date))->toDateString();
 }
-add_action('wp_head', 'construction_equipments_dynamic_colors');
+
+function rnb_get_duration($start, $end)
+{
+    $defaults = [
+        'duration' => 0,
+        'days'     => 0,
+        'hours'    => 0,
+    ];
+
+    if (empty($start) || empty($end)) {
+        return $defaults;
+    }
+
+    $mins      = $start->floatDiffInRealMinutes($end);
+    $durations = $mins / 60;
+    $day       = intval($mins / (24 * 60));
+    $hour      = (int) ceil($mins % (24 * 60) / 60);
+
+    if ($hour >= 24) {
+        $day = $day + 1;
+        $hour = $hour - 24;
+    }
+
+    return wp_parse_args([
+        'duration' => $durations,
+        'days'     => $day,
+        'hours'    => $hour
+    ], $defaults);
+}
+
+/**
+ * Default inventory id
+ *
+ * @return int
+ */
+function rnb_get_default_inventory_id($product_id = null)
+{
+    if (empty($product_id)) {
+        $product_id = get_the_ID();
+    }
+
+    $inventory_id = '';
+    $inventory_ids = rnb_get_product_inventory_id($product_id);
+
+    if (!empty($inventory_ids) && is_array($inventory_ids)) {
+        $inventory_id = $inventory_ids[0];
+    }
+
+    return $inventory_id;
+}
+
+/**
+ * item meta key to hold all data
+ *
+ * @return string
+ */
+function rnb_oder_item_data_key()
+{
+    return apply_filters('rnb_order_item_data_key', 'rnb_hidden_order_meta');
+}
+
+function rnb_format_oc_time($args, $conditions)
+{
+    $oc_times = $args['openning_closing'];
+
+    if (empty($oc_times)) {
+        return $args;
+    }
+
+    $formatted   = [];
+    $day_to_dow = rnb_get_day_of_dow();
+    $timeFormat = $conditions['time_format'] === '24-hours' ? 'H:i' : 'h:ia';
+
+    foreach ($oc_times as $key => $oc_time) {
+        $today = Carbon::now()->toDateString();
+        $min = (new Carbon($today . $oc_time['min']))->format($timeFormat);
+        $max_time =  $oc_time['max'] == '24:00' ? '23:59' : $oc_time['max'];
+        $max = (new Carbon($today .  $max_time))->format($timeFormat);
+        $new_key = $day_to_dow[$key];
+        $formatted[$new_key] = [
+            'min' => $min,
+            'max' => $max,
+        ];
+    }
+
+    $args['openning_closing'] = $formatted;
+
+    return $args;
+}
+
+/**
+ * Mapping date format
+ *
+ * @return array
+ */
+function rnb_map_date_format()
+{
+    return [
+        'm/d/Y' => 'mm/dd/yy',
+        'd/m/Y' => 'dd/mm/yy',
+        'Y/m/d' => 'yy/mm/dd'
+    ];
+}
+
+/**
+ * Get day of week
+ *
+ * @return array
+ */
+function rnb_get_day_of_dow()
+{
+    return [
+        'sun' => 0,
+        'mon' => 1,
+        'tue' => 2,
+        'wed' => 3,
+        'thu' => 4,
+        'fri' => 5,
+        'sat' => 6,
+    ];
+}
+
+/**
+ * Default pickup time
+ *
+ * @return string
+ */
+function rnb_get_default_pickup_time($product_id, $form_data = [])
+{
+    global $post;
+
+    if (empty($product_id)) {
+        $product_id = $post->ID;
+    }
+
+    $conditions = redq_rental_get_settings($product_id, 'conditions')['conditions'];
+    $display = redq_rental_get_settings($product_id, 'display')['display'];
+
+    $offset = (float) get_option('gmt_offset');
+    $today = (new Carbon())->addHours($offset);
+
+    $current_date = $today->toDateString();
+    $pickup_date = (new Carbon($form_data['pickup_date']))->toDateString();
+
+    if ($current_date !== $pickup_date) {
+        return '00:00';
+    }
+
+    $slots = [];
+    $interval = isset($conditions['time_interval']) && $conditions['time_interval'] ? $conditions['time_interval'] : 30;
+
+    for ($i = 1; $i <= 60; $i++) {
+        if ($i % $interval === 0) {
+            $slots[]  = $i;
+        }
+    }
+
+    $hour = $today->hour;
+
+    foreach ($slots as $key => $slot) {
+        $min = $slot;
+        if ($slot === 60) {
+            $hour = $hour + 1;
+            $min = 0;
+        }
+        $custom = "$current_date $hour:$min";
+        $customObject = new Carbon($custom);
+
+        if ($customObject->greaterThan($today)) {
+            return $customObject->format('H:i');
+        }
+    }
+
+    return (Carbon::now())->format('H:i');
+}
+
+/**
+ * Default return time
+ *
+ * @return string
+ */
+function rnb_get_default_return_time($product_id, $form_data = [])
+{
+    global $post;
+
+    if (empty($product_id)) {
+        $product_id = $post->ID;
+    }
+
+    $conditions = redq_rental_get_settings($product_id, 'conditions')['conditions'];
+    $display = redq_rental_get_settings($product_id, 'display')['display'];
+
+    $offset = (float) get_option('gmt_offset');
+    $today = (new Carbon())->addHours($offset);
+
+    $current_date = $today->toDateString();
+    $pickup_date = (new Carbon($form_data['pickup_date']))->toDateString();
+    $return_date = (new Carbon($form_data['return_date']))->toDateString();
+
+    if ($current_date === $return_date) {
+        return '23:59:59';
+    }
+
+    if ($pickup_date === $return_date) {
+        return '23:59:59';
+    }
+
+    if ($pickup_date !== $return_date && $conditions['include_trailing_date'] === 'closed') {
+        return '00:00';
+    }
+
+    if ($conditions['include_trailing_date'] !== 'closed') {
+        return '23:59:59';
+    }
+
+    return $today->format('H:i:s');
+}
+
+/**
+ * Parse weekend into init value
+ * 
+ * @param  array $weekend
+ * @return array
+ */
+function rnb_format_weekend($weekends)
+{
+    if (empty($weekends)) {
+        return [];
+    }
+
+    $results = [];
+
+    foreach ($weekends as $weekend) {
+        $results[] = intval($weekend);
+    }
+
+    return $results;
+}
+
+/**
+ * Get times slots
+ *
+ * @param integer $interval
+ * @param string $format
+ * @return array
+ */
+function rnb_get_time_slots($interval = 30, $format = 'H:i')
+{
+    $period = new CarbonPeriod('00:00', '' . $interval . ' minutes', '24:00');
+    $slots  = [];
+
+    foreach ($period as $item) {
+        array_push($slots, $item->format($format));
+    }
+
+    return $slots;
+}
+
+/**
+ * Check dates overlapping
+ *
+ * @param array $args1
+ * @param array $args2
+ * @return boolean
+ */
+function rnb_check_dates_overlap($args1, $args2)
+{
+    $pickup_date = $args1['pickup_date'];
+    $pickup_time = $args1['pickup_time'];
+    $return_date = $args1['return_date'];
+    $return_time = $args1['return_time'];
+
+    $pickup_datetime = $pickup_date . ' ' . $pickup_time;
+    $return_datetime = $return_date . ' ' . $return_time;
+
+    $pickup_date_2 = $args2['pickup_date'];
+    $pickup_time_2 = $args2['pickup_time'];
+    $return_date_2 = $args2['dropoff_date'];
+    $return_time_2 = $args2['dropoff_time'];
+
+    $pickup_datetime_2 = $pickup_date_2 . ' ' . $pickup_time_2;
+    $return_datetime_2 = $return_date_2 . ' ' . $return_time_2;
+
+    $period = CarbonPeriod::create($pickup_datetime, $return_datetime);
+    $period_2 = CarbonPeriod::create($pickup_datetime_2, $return_datetime_2);
+
+    return $period->overlaps($period_2);
+}
+
+
+// add_filter('woocommerce_product_class', 'load_custom_product_class', 10, 2);
+function load_custom_product_class($classname, $product_type)
+{
+    if ($product_type === 'redq_rental') {
+        $classname = 'WC_Product_Redq_rental';
+    }
+
+    return $classname;
+}
+
+if (!function_exists('is_view_quote_page')) {
+
+    /**
+     * Is_view_quote_page - Returns true when on the view order page.
+     *
+     * @return bool
+     */
+    function is_view_quote_page()
+    {
+        global $wp;
+
+        $page_id = wc_get_page_id('myaccount');
+
+        return ($page_id && is_page($page_id) && isset($wp->query_vars['view-quote']));
+    }
+}
+
+if (!function_exists('is_rfq_page')) {
+
+    /**
+     * Is_rfq_page - Returns true when on the view order page.
+     *
+     * @return bool
+     */
+    function is_rfq_page()
+    {
+        global $wp;
+
+        $page_id = wc_get_page_id('myaccount');
+
+        return ($page_id && is_page($page_id) && isset($wp->query_vars['view-quote'])) || ($page_id && is_page($page_id) && isset($wp->query_vars['request-quote']));
+    }
+}
+
+/**
+ * Retrieve Instance payment type
+ */
+function rnb_get_instance_payment_type()
+{
+    $instance_payment_type = get_option('rnb_instance_payment_type');
+    $instance_payment_type = empty($instance_payment_type) ? 'percent' : $instance_payment_type;
+    return $instance_payment_type;
+}
+
+function rnb_convert_dates_in_common_format($dates)
+{
+    if (empty($dates)) {
+        return [];
+    }
+
+    $formatted_dates = array_map(function ($date) {
+        return Carbon::parse($date)->format('Y-m-d');
+    }, $dates);
+
+    return $formatted_dates;
+}
